06e98a8014308f3a6bfe550df48ae32a4b5d5a43
nihalgopalam <nihalgopalam@gmail.com>
2021-11-08 22:57:18 -0600

    New Pages

--- app.py
+++ app.py
@@ -9,3 +9,13 @@ def index():
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/code")
+def code():
+    return render_template("code.html")
+
+@app.route("/recordings")
+def recordings():
+    return render_template("recordings.html")
+
+    
